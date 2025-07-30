77c20bcf8276e89b45a4449311c99c37a3916d28
Ohmmygod <ohmmygod787@gmail.com>
2025-07-30 11:49:28 +0700

    finished DSA Lab4

--- Week4/pack/Lab04_670764.java
+++ Week4/pack/Lab04_670764.java
@@ -40,60 +40,66 @@ public class Lab04_670764 {
         System.out.println("(k= " + 1 + ") -> " + lis); 
         System.out.println("==================");
     }
-    // static void q2() {
-    //     int [] d = {1,2,3,4,5,6,7,8};
-    //     MyTrickyLinkedList lis = new MyTrickyLinkedList();
-    //     lis.insert(d);
-    //     System.out.println("before -> " + lis);
-    //     lis.q2_reverse(); 
-    //     System.out.println( lis );       
-    // }
-    // static void q3() {
-    //     int [] d = {13,11,4,15,4};
-    //     MyTrickyLinkedList lis = new MyTrickyLinkedList();
-    //     lis.insert(d);
-    //     System.out.println("before -> " + lis);
-    //     lis.q3_remove_dup();
-    //     System.out.println("after-> " + lis);
-    //     int [] e = {13,11,15,4};
-    //     lis = new MyTrickyLinkedList();
-    //     lis.insert(e);
-    //     System.out.println("before -> " + lis);
-    //     lis.q3_remove_dup();
-    //     System.out.println("after-> " + lis);
-    // }
-    // static void q4() {
-    //     int [] d = {1,9,9,9};
-    //     MyTrickyLinkedList lis = new MyTrickyLinkedList();
-    //     lis.insert(d);
-    //     System.out.println("before -> " + lis);
-    //     lis.q4_add_one();
-    //     System.out.println("after-> " + lis);
-    //     int [] e = {9,9,9,9};
-    //     lis = new MyTrickyLinkedList();
-    //     lis.insert(e);
-    //     System.out.println("before -> " + lis);
-    //     lis.q4_add_one();
-    //     System.out.println("after-> " + lis);
-    // }
-    // static void q5() {
-    //     boolean isTrue;
-    //     int [] d = {21, 33, 33, 21};
-    //     MyTrickyLinkedList lis = new MyTrickyLinkedList();
-    //     lis.add(d);
-    //     isTrue = lis.q5_isPalindrome();
-    //     System.out.println(lis + " isPalindrome= " + isTrue);
-    //     int [] e = {21,33,44,33,21};
-    //     lis = new MyTrickyLinkedList();
-    //     lis.add(e);
-    //     isTrue = lis.q5_isPalindrome();
-    //     System.out.println(lis + " isPalindrome= " + isTrue);
-    //     int [] f = {1,9,9,9};
-    //     lis = new MyTrickyLinkedList();
-    //     lis.add(f);
-    //     isTrue = lis.q5_isPalindrome();
-    //     System.out.println(lis + " isPalindrome= " + isTrue);
-    // }
+    static void q2() {
+        int [] d = {1,2,3,4,5,6,7,8};
+        MyTrickyLinkedList lis = new MyTrickyLinkedList();
+        lis.insert(d);
+        System.out.println("before -> " + lis);
+        lis.q2_reverse(); 
+        System.out.println( lis );       
+        System.out.println("==================");
+    }
+    static void q3() {
+        int [] d = {13,11,4,15,4};
+        MyTrickyLinkedList lis = new MyTrickyLinkedList();
+        lis.insert(d);
+        System.out.println("before -> " + lis);
+        lis.q3_remove_dup();
+        System.out.println("after-> " + lis);
+        int [] e = {13,11,15,4};
+        lis = new MyTrickyLinkedList();
+        lis.insert(e);
+        System.out.println("before -> " + lis);
+        lis.q3_remove_dup();
+        System.out.println("after-> " + lis);
+        System.out.println("==================");
+    }
+    static void q4() {
+        int [] d = {1,9,9,9};
+        MyTrickyLinkedList lis = new MyTrickyLinkedList();
+        lis.insert(d);
+        System.out.println("before -> " + lis);
+        lis.q4_increment_digits();
+        System.out.println("after-> " + lis);
+        int [] e = {9,9,9,9};
+        lis = new MyTrickyLinkedList();
+        lis.insert(e);
+        System.out.println("before -> " + lis);
+        lis.q4_increment_digits();
+        System.out.println("after-> " + lis);
+        System.out.println("==================");
+
+    }
+    static void q5() {
+        boolean isTrue;
+        int [] d = {21, 33, 33, 21};
+        MyTrickyLinkedList lis = new MyTrickyLinkedList();
+        lis.add(d);
+        isTrue = lis.q5_isPalindrome();
+        System.out.println(lis + " isPalindrome= " + isTrue);
+        int [] e = {21,33,44,33,21};
+        lis = new MyTrickyLinkedList();
+        lis.add(e);
+        isTrue = lis.q5_isPalindrome();
+        System.out.println(lis + " isPalindrome= " + isTrue);
+        int [] f = {1,9,9,9};
+        lis = new MyTrickyLinkedList();
+        lis.add(f);
+        isTrue = lis.q5_isPalindrome();
+        System.out.println(lis + " isPalindrome= " + isTrue);
+        System.out.println("==================");
+
+    }
 
 
 
@@ -104,14 +110,14 @@ public class Lab04_670764 {
         demo_2(); 
         System.out.println("calling q1");
         q1();
-        // System.out.println("calling q2");
-        // q2();
-        // System.out.println("calling q3");
-        // q3();
-        // System.out.println("calling q4");
-        // q4();        
-        // System.out.println("calling q5");
-        // q5(); 
+        System.out.println("calling q2");
+        q2();
+        System.out.println("calling q3");
+        q3();
+        System.out.println("calling q4");
+        q4();        
+        System.out.println("calling q5");
+        q5(); 
 
     //     MyLinkedList_670764 obj1 = new MyLinkedList_670764();
     //     MyLinkedList_670764 obj2 = new MyLinkedList_670764();
--- Week4/pack/MyTrickyLinkedList.java
+++ Week4/pack/MyTrickyLinkedList.java
@@ -29,20 +29,77 @@ public class MyTrickyLinkedList  extends MyLinkedList_670764 {
 	}
 
 	public void q2_reverse() {
-		// int	idx = size - 1;
+		Node prev = null;
+		Node run = head;
+		Node tmp = null;
 
-
+		while (run != null)
+		{
+			tmp = run.next;
+			run.next = prev;
+			prev = run;
+			run = tmp;
+		}
+		head = prev;
 	}
 
 	public void q3_remove_dup() {
-
+		Node run = head;
+		while (run != null && run.next != null)
+		{
+			if (run.data == run.next.data)
+				run.next = run.next.next;
+			else
+				run = run.next;
+		}
 	}
 
 	public void q4_increment_digits() {
-
+		Node notNine = null;
+		Node run = head;
+		while (run != null) {
+			if (run.data != 9)
+				notNine = run;
+			run = run.next;
+		}
+		if (notNine == null) 
+		{
+			Node newHead = new Node(1);
+			newHead.next = head;
+			head = newHead;
+			notNine = head;
+		} 
+		else
+			notNine.data++;
+		run = notNine.next;
+		while (run != null) {
+			run.data = 0;
+			run = run.next;
+		}
 	}
 
 	public boolean q5_isPalindrome() {
-		return false;
+		int n = size();
+		if (n == 0) 
+			return true;
+
+		int[] arr = new int[n];
+		Node run = head;
+		int idx = 0;
+		while (run != null)
+		{
+			arr[idx++] = run.data;
+			run = run.next;
+		}
+
+		int i = 0, j = n - 1;
+		while (i < j)
+		{
+			if (arr[i] != arr[j])
+				return false;
+			i++;
+			j--;
+		}
+		return true;
 	}
 }
