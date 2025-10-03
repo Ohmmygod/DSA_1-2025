5e3be91d53aa83a3677c4435bf8220e8acd21e4f
Ohmmygod <ohmmygod787@gmail.com>
2025-10-03 15:18:50 +0700

    Lab12ab 45%

---
+++ Week13/Lab12ab/Lab12a_Graph1_670764.java
@@ -0,0 +1,118 @@
+package Lab12ab;
+
+import Lab12ab.pack.GraphMap_670764;
+
+public class Lab12a_Graph1_670764 {
+    final static int INF = Integer.MAX_VALUE;   // for q2
+  
+    public static void main(String args[]) {
+        // q1_1();
+        q1_2();
+        // q1_3();
+        // q2();
+        // q3_1();
+        // q3_2();
+        // q3_3();
+        // q3_4();
+    }
+
+    // static void q1_1() {
+    //     System.out.println("Graph List");
+    //     GraphList_670764 graphL = new GraphList_670764(5);
+    //     graphL.addEdge(0, 1);
+    //     graphL.addEdge(0, 4);
+    //     graphL.addEdge(1, 2);
+    //     graphL.addEdge(1, 3);
+    //     graphL.printGraph();
+    //         // 0 -> 1 4 
+    //         // 1 -> 0 2 3
+    //         // 2 -> 1
+    //         // 3 -> 1
+    //         // 4 -> 0        
+    //     System.out.println("From 1 to 3 = " + graphL.hasEdge(1, 3));   // true
+    //     System.out.println("From 1 to 0 = " + graphL.hasEdge(1, 0));   // true
+    //     System.out.println("From -3 to 0 = " + graphL.hasEdge(-3, 0));   //  false
+    //     System.out.println("Is there a path from 4 to 2 = "+ graphL.hasPath(4, 2) );  // true
+    //     System.out.println("Is there a path from 0 to 3 = "+ graphL.hasPath(0, 3) );  // true
+    //     System.out.println("Is there a path from 4 to 1 = "+ graphL.hasPath(4, 1) );  // true
+    //     System.out.println("Neighbor of [2] = " + graphL.neighborsOf(2) );     // [1]
+    //     System.out.println("Neighbor of [0] = " + graphL.neighborsOf(0) );     // [1, 4]
+
+    //     System.out.println("DFS:");
+    //     graphL.dfs(0); // 0 1 2 3 4
+    // }
+    static void q1_2() {
+        System.out.println("Graph Map");
+        GraphMap_670764 graphM = new GraphMap_670764();
+        graphM.addEdge(1, 2);
+        graphM.addEdge(1, 3);
+        graphM.addEdge(2, 4);
+        graphM.printGraph();
+            // 1 -> [2, 3]
+            // 2 -> [1, 4]
+            // 3 -> [1]
+            // 4 -> [2]      
+        System.out.println("hasEdge from 1 to 4: " + graphM.hasEdge(1, 4));   // false
+        System.out.println("hasEdge from 4 to 2: " + graphM.hasPath(4, 2) );  // true
+        // graphM.addVertex(5);    // add isolated node 5 to the graph
+        // System.out.println(graphM.hasPath(5, 2) );  // false
+        System.out.println("***"+ graphM.neighborsOf(2) ); // [1, 4]
+        System.out.println("Neighbors of [5] while graph size = 4: "  + graphM.neighborsOf(5) );
+
+        // System.out.println("DFS:");
+        // graphM.dfsAll();  
+        // // 1 2 4 3
+        // // 5
+    }
+    // static void q1_3() {
+    //     System.out.println("Graph Node");
+    //     GraphNode_670764 graphN = new GraphNode_670764();
+    //     Node a = graphN.addNode(1);
+    //     Node b = graphN.addNode(2);
+    //     Node c = graphN.addNode(3);
+
+    //     graphN.addEdge(a, b);
+    //     graphN.addEdge(b, c);
+    //     graphN.addEdge(a, c);
+
+    //     graphN.printGraph();
+    //         // 1 -> 2 3
+    //         // 2 -> 1 3
+    //         // 3 -> 2 1
+    //     System.out.println(graphN.hasEdge(1, 3));   // true
+    //     System.out.println(graphN.hasPath(3, 2) );  // true
+    //     System.out.println("***"+ graphN.neighborsOf(2) ); // [1, 3]
+
+    //     System.out.println("DFS:");
+    //     graphN.dfs(a);  // 1 2 3
+    // }
+
+    // static void q2() {
+        
+    //     int[][] thisGraph = {{0,3,INF,INF,INF},
+    //                             {INF,0,1,INF,INF},
+    //                             {INF,INF,0,4,INF},
+    //                             {INF,INF,INF,0,5},
+    //                             {INF,INF,INF,INF,0}};
+    //     System.out.println("computing dfs");
+    //     q2_dfs(thisGraph);
+    // }
+
+    // private static void q2_dfs(int[][] thisGraph) {
+    //     ArrayList<Integer> stack = new ArrayList<>();
+    //     ArrayList<Integer> visited = new ArrayList<>();
+        
+    //     stack.add(0);
+    //     while (!stack.isEmpty()) {
+    //         int parent = 0; /* your code 9 */
+    //         visited.add(parent);
+    //         for (int x = 0; x < thisGraph.length; x++) {
+    //             if (0 < thisGraph[parent][x] && thisGraph[parent][x] < INF /* && your code 10 */) { 
+    //                 stack.add(x);
+    //                 System.out.println("Edge " + parent + ", " + x);
+    //             }
+    //         }
+
+    //     } // while 
+    // }
+}
---
+++ Week13/Lab12ab/pack/GraphMap_670764.java
@@ -0,0 +1,143 @@
+package Lab12ab.pack;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class GraphMap_670764 {
+                // 1    [2, 3]
+    private Map<Integer, List<Integer>> graph;
+
+    public GraphMap_670764() {
+        graph = new HashMap<>();
+    }
+
+    public void addVertex(int v) {
+        graph.putIfAbsent(v, new ArrayList<>());
+    }
+                        //   1         2
+    public void addEdge(int src, int dest) {
+        graph.putIfAbsent(src, new ArrayList<>());
+        graph.putIfAbsent(dest, new ArrayList<>());
+        /* your code 3 */
+        graph.get(src).add(dest);
+        graph.get(dest).add(src); // remove if directed
+    }
+    
+			// 1 -> [2, 3]
+            // 2 -> [1, 4]
+            // 3 -> [1]
+            // 4 -> [2]  
+    public List<Integer> neighborsOf(int i) {
+        /* your code 4 */
+		// List<Integer> neighbors = new ArrayList<>();
+		// for (int n : graph.get(i))
+		// 	neighbors.add(n);
+		// return (neighbors);
+
+		if (i >= 0 && i <= graph.size())
+			return (graph.get(i));
+
+        System.out.println("out of range"); 
+        return Collections.emptyList();
+    }
+
+    public boolean hasEdge(int n1, int n2) {
+        if (!graph.containsKey(n1) || !graph.containsKey(n2)) {
+            return false;
+        }
+        return graph.get(n1).contains(n2); /* <- your code 5 */
+    }
+
+	public boolean hasPath(int n1, int n2) {
+		if (!graph.containsKey(n1) || !graph.containsKey(n2)) {
+			return false;
+		}
+		Set<Integer> visited = new HashSet<>();
+		return hasPathDFS(n1, n2, visited);
+    }
+
+    private boolean hasPathDFS(int current, int target, Set<Integer> visited) {
+        if (current == target) return true;
+        visited.add(current);
+
+        for (int neighbor : graph.get(current)) {
+            if (!visited.contains(neighbor)) {
+                if (hasPathDFS(neighbor, target, visited)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void dfs(int src) {
+        if (!graph.containsKey(src)) {
+            System.out.println("Error: Node " + src + " does not exist in this graph.");
+            return;
+        }
+        Set<Integer> visited = new HashSet<>();
+        dfsHelper(src, visited);
+        System.out.println("");
+    }
+
+    private void dfsHelper(int v, Set<Integer> visited) {
+        visited.add(v);
+        System.out.print(v + " ");
+
+        for (int neighbor : graph.get(v)) {
+            if (!visited.contains(neighbor)) {
+                dfsHelper(neighbor, visited);
+            }
+        }
+    }
+
+    public void dfsAll() {
+        Set<Integer> visited = new HashSet<>();
+        for (int v : graph.keySet()) {
+            if (!visited.contains(v)) {
+                /* your code 6 */
+				
+                System.out.println(); // newline after each component
+            }
+        }
+    }    
+
+    public void printGraph() {
+        for (int v : graph.keySet()) {
+            System.out.println(v + " -> " + graph.get(v));
+        }
+    }
+public boolean hasCycle() {
+    Set<Integer> visited = new HashSet<>();
+
+    for (int v : graph.keySet()) {
+        if (!visited.contains(v)) {
+            if (hasCycleDFS(v, -1, visited)) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+private boolean hasCycleDFS(int current, int parent, Set<Integer> visited) {
+    visited.add(current);
+
+    for (int neighbor : graph.get(current)) {
+        if (!visited.contains(neighbor)) {
+            // explore deeper
+            if (true /* your code 10 */) {
+                return true;
+            }
+        } else if (neighbor != parent) {
+            // found a back edge -> cycle
+            return false /* your code 11 */;
+        }
+    }
+    return false;
+}  
+}
